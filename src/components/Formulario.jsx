fc0a58ca5cbbfb92392392739f6ce276aa67d4a1
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-05-22 10:00:08 -0300

    logica de tarea

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,17 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import ListaTarea from './ListaTarea';
+import { useState } from 'react';
 
 const Formulario = () => {
+  const [tarea, setTarea] = useState("");
+  const [listadoTareas, setListadoTareas] = useState([]);
+
   return (
     <section>
       <Form>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+          <Form.Control type="text" placeholder="Ingrese una tarea" onChange={(e)=>setTarea(e.target.value)} value={tarea}/>
           <Button variant="primary" type="submit">
           Submit
         </Button>
